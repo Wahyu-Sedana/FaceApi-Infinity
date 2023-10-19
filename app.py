a1e50642d04b3e43ea3f634a4ffbfe888b1517ce
WahyuSedana <wsstudiocode@gmail.com>
2023-10-19 10:35:13 +0800

    add ecosystem.config.js

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from routes.route import face
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "https://absensi-staging.infinityprogress.id"}})
 app.register_blueprint(face)
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+      {
+        name: 'face',  
+        script: 'app.py',  
+        interpreter: 'python3',
+      }
+    ]
+};
+  
