4e0316755538841ef9898026084997de3cfc66f9
WahyuSedana <wsstudiocode@gmail.com>
2023-10-19 13:27:55 +0800

    add database file and update app.py cors

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from routes.route import face
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://absensi-staging.infinityprogress.id"}})
-app.config['CORS_SUPPORTS_CREDENTIALS'] = True
 app.register_blueprint(face)
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_SUPPORTS_CREDENTIALS'] = True
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- helper/manipulation-text.sh
+++ helper/manipulation-text.sh
@@ -3,9 +3,9 @@ echo "from routes.route import face" >> app.py
 echo "from flask_cors import CORS" >> app.py
 echo "" >> app.py
 echo "app = Flask(__name__)" >> app.py
-echo "app.register_blueprint(face)" >> app.py
-echo "CORS(app, resources={r\"/*\": {\"origins\": \"https://absensi-staging.infinityprogress.id\"}})" >> app.py
+echo "CORS(app, resources={r\"/*\": {\"origins\": \"*\"}})" >> app.py
 echo "app.config['CORS_SUPPORTS_CREDENTIALS'] = True" >> app.py
+echo "app.register_blueprint(face)" >> app.py
 echo "" >> app.py
 echo "if __name__ == \"__main__\":" >> app.py
 echo "    app.run(debug=True, port=5000)" >> app.py
